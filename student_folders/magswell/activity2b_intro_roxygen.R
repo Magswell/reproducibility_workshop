e8c4f311dfc7eca176279be32bd8077e5cfd6590
Magswell <edwar486@umn.edu>
2017-07-23 14:58:27 -0500

    Activity 2 from workshop

---
+++ student_folders/magswell/activity2b_intro_roxygen.R
@@ -0,0 +1,26 @@
+#' # Linear regression of RIKZ data
+
+#' Preamble - load libraries and clear environment
+remove(list=ls())
+
+#' ## Step 1. Load data
+load("data/processed_data/rikz.Rdata")
+
+#' ## Step 2. Linear regression
+lm.out <- lm(Richness~NAP, data = rikz.data)
+summary(lm.out)
+
+#' ## Step 3. Plot results
+#+ rikz_lm, fig.width=8, fig.height=5
+plot(rikz.data$NAP, rikz.data$Richness)
+abline(lm.out)
+
+#' ### Footer
+#' 
+#' Session Information
+devtools::session_info()
+#'
+ezspin("student_folders/magswell/activity2b_intro_roxygen.R", 
+ out_dir = "student_folders/magswell/reports", 
+ fig_dir = "figures",
+ keep_md=F)
